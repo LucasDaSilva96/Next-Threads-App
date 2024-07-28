3477c68ee51a364360b7bac7004e1a2e1aa63f1a
LucasDaSilva96 <dasilvajunior881@gmail.com>
2024-07-28 14:08:46 +0200

    First commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,24 @@
+import { ClerkProvider } from '@clerk/nextjs';
+import { Inter } from 'next/font/google';
+import '../globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata = {
+  title: 'Threads',
+  description: 'A Next.js Meta Threads Application',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang='en'>
+        <body className={`${inter.className} bg-dark-1`}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+}
